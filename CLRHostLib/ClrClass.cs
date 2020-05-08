9b3750ee37f87fd761368234ddaeefa5e878a316
NIMO <laomms@gmail.com>
2020-05-08 07:56:21 +0700

    添加项目文件。

---
+++ CLRHostLib/ClrClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CLRHostLib
+{
+    public class ClrClass
+    {
+        public static int ClrExcute(string paramstr)
+        {
+            MessageBox.Show(paramstr);
+            return 0;
+        }
+
+    }
+}
---
+++ CallDonet/CallDonet.cpp
@@ -0,0 +1,23 @@
+﻿#include <metahost.h>
+#pragma comment(lib, "mscoree.lib")
+
+int main()
+{
+    ICLRMetaHost* metaHost = NULL;
+    ICLRRuntimeInfo* runtimeInfo = NULL;
+    ICLRRuntimeHost* runtimeHost = NULL;
+
+    if (CLRCreateInstance(CLSID_CLRMetaHost, IID_ICLRMetaHost, (LPVOID*)&metaHost) == S_OK)
+        if (metaHost->GetRuntime(L"v4.0.30319", IID_ICLRRuntimeInfo, (LPVOID*)&runtimeInfo) == S_OK)
+            if (runtimeInfo->GetInterface(CLSID_CLRRuntimeHost, IID_ICLRRuntimeHost, (LPVOID*)&runtimeHost) == S_OK)
+                if (runtimeHost->Start() == S_OK)
+                {
+                    DWORD pReturnValue;
+                    runtimeHost->ExecuteInDefaultAppDomain(L"D:\\HOOK\\CallDonet\\CLRHostLib\\bin\\Debug\\CLRHostLib.dll", L"CLRHostLib.ClrClass", L"ClrExcute", L"It works!!", &pReturnValue);
+
+                    runtimeInfo->Release();
+                    metaHost->Release();
+                    runtimeHost->Release();
+                }
+    return 0;
+}
